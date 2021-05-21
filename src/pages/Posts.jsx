61c142c7fc771ed8da02524e13328a88d3e4b4c3
Daniel <danielanugr@yahoo.co.id>
2021-05-21 13:06:37 +0700

    create config for tailwind and create sidebar component

---
+++ src/pages/Posts.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export default function Posts () {
+  return (
+    <>
+      <main className='flex-1 relative z-0 overflow-y-auto focus:outline-none'>
+        <div className='py-6'>
+          <div className='max-w-7xl mx-auto px-4 sm:px-6 md:px-8'>
+            <h1 className='text-2xl font-semibold text-gray-900'>
+              Blogpost Page
+            </h1>
+          </div>
+          <div className='max-w-7xl mx-auto px-4 sm:px-6 md:px-8'>
+            {/* Replace with your content */}
+            <div className='py-4'>
+              <div className='border-4 border-dashed border-gray-200 rounded-lg h-96' />
+            </div>
+            {/* /End replace */}
+          </div>
+        </div>
+      </main>
+    </>
+  )
+}
